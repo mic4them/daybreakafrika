11564e591bed3a3a323dfb87fd0301f6771cf253
micheal <mic4them@yahoo.com>
2017-12-02 20:24:53 +0100

    indexandcontact

---
+++ php/sendmail.php
@@ -0,0 +1,76 @@
+<?php 
+
+$message_array = array();
+
+if(isset($_POST['formid']) && $_POST['formid']=='planner'){
+	$subject = "Quote Request via Project Planner";
+
+	if(empty($_POST['name'])) $message_array[] = 'Name';
+	if(empty($_POST['email'])) $message_array[] = 'Email';
+	if(empty($_POST['project-details'])) $message_array[] = 'Project Details';
+	$body = 'Name: '.$_POST['name'].'<br>';
+	$body .= 'Email: '.$_POST['email'].'<br>';
+	$body .= 'Phone: '.$_POST['phone'].'<br>';
+	$body .= 'project-details: '.$_POST['project-details'].'<br>';
+	// $body .= 'file-attachment: '.$_POST['file-attachment'].'<br>';
+	// $body .= 'project-type: '.$_POST['project-type'].'<br>';
+	$body .= 'start-date: '.$_POST['start-date'].'<br>';
+	$body .= 'due-date: '.$_POST['due-date'].'<br>';
+	$body .= 'budget: '.$_POST['budget'].'<br>';
+}
+else{
+	$subject = "Question via Contact Form";
+
+	if(empty($_POST['name'])) $message_array[] = 'Name';
+	if(empty($_POST['email'])) $message_array[] = 'Email';
+	if(empty($_POST['subject'])) $message_array[] = 'Subject';
+	if(empty($_POST['message'])) $message_array[] = 'Message';
+	$body = 'Name: '.$_POST['name'].'<br>';
+	$body .= 'Email: '.$_POST['email'].'<br>';
+	$body .= 'Subject: '.$_POST['subject'].'<br>';
+	$body .= 'Message: '.$_POST['message'].'<br>';
+}
+
+
+if(sizeof($message_array)==1){
+	$message = $message_array[0].' is required field.';
+}
+elseif(sizeof($message_array)>1){
+	$message = join(' and ', array_filter(array_merge(array(join(', ', array_slice($message_array, 0, -1))), array_slice($message_array, -1)))). ' are required fields.';
+	// $message = implode(', ', $message_array). ' are required fields.';
+}
+else{
+	$message = '';
+}
+
+
+if($message=='' && (isset($_POST['action']) && $_POST['action']=='js')){
+	require_once('class.phpmailer.php');
+	include("class.smtp.php");
+
+	$mail = new PHPMailer();
+
+	$mail->IsSMTP(); 							// telling the class to use SMTP
+	$mail->SMTPDebug  = 0;                     	// enables SMTP debug information (for testing)
+	$mail->SMTPAuth   = true;                  	// enable SMTP authentication
+	$mail->SMTPSecure = "tls";                 	// sets the prefix to the server
+	$mail->Host       = "smtp.example.com";      	// sets GMAIL as the SMTP server
+	$mail->Port       = 587;                   	// set the SMTP port for the GMAIL server
+	$mail->Username   = "user@example.com"; 	// GMAIL username
+	$mail->Password   = "";          	// GMAIL password
+	$mail->SetFrom('from@example.com', 'Mailer');
+
+	$mail->Subject = $subject;
+	$mail->MsgHTML($body);
+	$mail->AddAddress("joe@example.net", "Joe User");
+
+	if(!$mail->Send()) {
+	  // echo "Mailer Error: " . $mail->ErrorInfo;
+	  echo "Error while sending message...";
+	} else {
+	  echo "OK";
+	}
+}
+else{
+	echo $message;
+}
